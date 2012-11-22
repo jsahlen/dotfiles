e64ae42c33459c5955a127b797f874749d224556
Johan Sahlén <johan.sahlen@gmail.com>
2012-11-22 14:40:01 +0100

    Prevent Sickbeard rename script to run if we downloaded a whole season

--- bin/sickbeard/rename_episode.rb
+++ bin/sickbeard/rename_episode.rb
@@ -11,13 +11,14 @@ result_filename = "#{dirname}#{sb_path.extname}"
 temp_path = sb_path.parent.join("#{result_filename}.tmp")
 final_path = sb_path.parent.join(result_filename)
 
+episode_match = /S\d{1,2}E\d{1,2}/i.match(dirname)
 group_match = /-(.{2,})$/i.match(dirname)
 
-if group_match && !%w{x264 h264 264}.include?(group_match[1].downcase)
+if episode_match && group_match && !%w{x264 h264 264}.include?(group_match[1].downcase)
   sb_path.rename(temp_path)
   temp_path.rename(final_path)
 
   puts "Renamed to #{final_path.basename}"
 else
-  puts "Looks like #{dirname} has no group; not renaming"
+  puts "Looks like #{dirname} has no group or is not a single episode; not renaming"
 end
