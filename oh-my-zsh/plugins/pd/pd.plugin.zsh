5adce09d29a666ce53df4ea955423bbafea5e6d5
Johan Sahlén <johan@monospace.se>
2014-05-22 15:00:19 +0200

    Remove old oh-my-zsh install

--- oh-my-zsh/plugins/pd/pd.plugin.zsh
+++
@@ -1,9 +0,0 @@
-# pro is a command to help you manage your git repositories.
-# --> gem install pro
-
-# This function does a pro search and cd's into the found directory.
-
-pd() {
-  local projDir=$(pro search $1)
-  cd ${projDir}
-}
