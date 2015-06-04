bd15d21eec130b1c93f07f9560b91082fae78165
Johan Sahlén <johan@monospace.se>
2015-06-04 10:31:11 +0200

    Rename dotenv function to avoid conflicts

--- oh-my-zsh-custom/plugins/dotenv/dotenv.plugin.zsh
+++ oh-my-zsh-custom/plugins/dotenv/dotenv.plugin.zsh
@@ -1,12 +1,12 @@
 #!/bin/zsh
 # https://github.com/antonio/zsh-config/blob/master/dotenv.zsh
 
-dotenv () {
+dotenv-enter-dir () {
   if [[ -a $PWD/.env ]]; then
     source $PWD/.env
   fi
 }
 
 function chpwd() {
-  dotenv
+  dotenv-enter-dir
 }
