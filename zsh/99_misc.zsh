6d74f82b7dba92f12a5c1e93c59930e91e6aad9e
Johan Sahlén <johan@monospace.se>
2017-01-04 15:02:53 +0100

    Use nvr as $EDITOR inside neovim terminals

--- zsh/99_misc.zsh
+++ zsh/99_misc.zsh
@@ -1,2 +1,7 @@
 # Set EDITOR to Neovim if available
 hash nvim &>/dev/null && export EDITOR=nvim
+
+# Set EDITOR to Neovim-remote if inside Neovim terminal
+if [ -n "$NVIM_TERMINAL" ]; then
+  export EDITOR="nvr --remote-tab-wait"
+fi
