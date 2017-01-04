6908ef17e9e2b0c2c1412127198fd65beccd03d5
Johan Sahlén <johan@monospace.se>
2017-01-04 15:02:24 +0100

    Don't use oh-my-zsh zplug plugins

--- zsh/zplug.zsh
+++ zsh/zplug.zsh
@@ -11,9 +11,7 @@ zplug "~/.zsh", \
   use:"<->_*.zsh", \
   defer:1
 
-zplug "plugins/rbenv", from:oh-my-zsh
-zplug "plugins/bundler", from:oh-my-zsh
-
 zplug "jsahlen/nodenv.plugin.zsh"
+zplug "jsahlen/rbenv.plugin.zsh"
 zplug "zsh-users/zsh-completions"
 zplug "zsh-users/zsh-history-substring-search", defer:2
