7ab68bcd635a5b4d0fd366daaa8d7c47484c44b5
Johan Sahlén <johan@monospace.se>
2017-01-04 15:01:56 +0100

    Compatibility with latest zplug

--- zsh/zplug.zsh
+++ zsh/zplug.zsh
@@ -4,16 +4,16 @@ zplug "~/.zsh/vendor", \
   from:local, \
   use:"*.sh", \
   use:"*.zsh", \
-  nice:1
+  defer:0
 
 zplug "~/.zsh", \
   from:local, \
   use:"<->_*.zsh", \
-  nice:2
+  defer:1
 
 zplug "plugins/rbenv", from:oh-my-zsh
 zplug "plugins/bundler", from:oh-my-zsh
 
 zplug "jsahlen/nodenv.plugin.zsh"
 zplug "zsh-users/zsh-completions"
-zplug "zsh-users/zsh-history-substring-search", nice:19
+zplug "zsh-users/zsh-history-substring-search", defer:2
