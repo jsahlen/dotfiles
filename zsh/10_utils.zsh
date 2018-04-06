00658dd370a38077cf84577806a89f4b42d3ff19
Johan Sahlén <johan@monospace.se>
2018-04-06 16:28:58 +0200

    shell: Add "tmuxproject" shortcut

--- zsh/10_utils.zsh
+++ zsh/10_utils.zsh
@@ -3,3 +3,11 @@ mkcd () {
   mkdir -p "$*"
   cd "$*"
 }
+
+# Quickly open a tmux "project" with 3 windows: server, editor, and shell
+if hash tmux &>/dev/null; then
+  tmuxproject () {
+    local session="${1:-$(basename $(pwd))}"
+    tmux new -s "${session}" \; rename-window server \; new-window -n editor \; new-window -n shell \; select-window -t 1
+  }
+fi
