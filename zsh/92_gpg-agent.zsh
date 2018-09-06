5d005d289ced66509c3ade86efc86ae435627745
Johan Sahlén <johan@monospace.se>
2018-09-06 13:53:23 +0200

    zsh: Make gpg-agent init more portable

--- zsh/92_gpg-agent.zsh
+++ zsh/92_gpg-agent.zsh
@@ -2,5 +2,5 @@
 hash gpgconf &>/dev/null
 if [ -z "$SSH_TTY" ] && [ $? = 0 ]; then
   gpgconf --launch gpg-agent > /dev/null 2>&1
-  export SSH_AUTH_SOCK=${GNUPGHOME:-$HOME/.gnupg}/S.gpg-agent.ssh
+  export SSH_AUTH_SOCK=$(gpgconf --list-dirs agent-ssh-socket)
 fi
