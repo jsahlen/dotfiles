105b082c0cdc30609e6872ff6dd01cbb9d695a53
Johan Sahlén <johan.sahlen@oktavilla.se>
2022-02-02 14:20:28 +0100

    zsh: Set EDITOR to code if inside vscode terminal

--- zsh/99_misc.zsh
+++ zsh/99_misc.zsh
@@ -2,3 +2,8 @@
 if [ -n "$NVIM_TERMINAL" ]; then
   export EDITOR="nvr --remote-tab-wait"
 fi
+
+# Set EDITOR to code if inside vscode terminal
+if [ -n "$VSCODE_GIT_IPC_HANDLE" ]; then
+  export EDITOR="code --wait"
+fi
