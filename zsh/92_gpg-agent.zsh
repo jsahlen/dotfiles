76bd570f13888f70522e1d5c977936554f7ad721
Johan Sahlén <johan.sahlen@oktavilla.se>
2022-02-02 14:20:06 +0100

    zsh: Remove gpg-agent support

--- zsh/92_gpg-agent.zsh
+++
@@ -1,6 +0,0 @@
-# If gpgconf is available, launch gpg-agent automatically and use as ssh-agent
-hash gpgconf &>/dev/null
-if [ -z "$SSH_TTY" ] && [ $? = 0 ]; then
-  gpgconf --launch gpg-agent > /dev/null 2>&1
-  export SSH_AUTH_SOCK=$(gpgconf --list-dirs agent-ssh-socket)
-fi
