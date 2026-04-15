6cc79d51d846f692763ef280a89e6ab394a26dc2
Johan Sahlén <johan@monospace.se>
2026-04-15 14:00:39 +0200

    Use zed as editor inside zed

--- zsh/99_misc.zsh
+++ zsh/99_misc.zsh
@@ -8,6 +8,12 @@ if [ -n "$VSCODE_GIT_IPC_HANDLE" ]; then
   export EDITOR="code --wait"
 fi
 
+# Set EDITOR to zed if inside zed terminal
+if [ -n "$ZED_TERM" ]; then
+  export EDITOR="zed --wait"
+  export GIT_EDITOR="zed-git"
+fi
+
 # Use bat as a colorizing pager for man
 if hash bat &>/dev/null; then
   export MANPAGER="sh -c 'col -bx | bat -l man -p'"
