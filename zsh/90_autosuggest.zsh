1597f213405bb852deb06fd5b23a879281923da0
Johan Sahlén <johan@monospace.se>
2026-04-15 13:59:46 +0200

    zsh: Update autosuggest config

--- zsh/90_autosuggest.zsh
+++ zsh/90_autosuggest.zsh
@@ -1,3 +1,4 @@
 export ZSH_AUTOSUGGEST_STRATEGY=(completion history)
-export ZSH_AUTOSUGGEST_HISTORY_IGNORE=(cd *)
+export ZSH_AUTOSUGGEST_HISTORY_IGNORE="(cd *|ls *|l *|cat *|man *|rm *|mv *|chmod *|cp *|rmdir *|?(#c100,))"
+export ZSH_AUTOSUGGEST_COMPLETION_IGNORE="(ssh *|scp *|ansible-playbook *)"
 
